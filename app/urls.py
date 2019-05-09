5542c27b4e8d078fa8d16f521e496e109386e594
zs <zhengsheng0118@163.com>
2019-05-09 19:40:25 +0800

    init

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import re_path
 from app import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category')
 ]
